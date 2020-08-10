3219741b4d03533ac16dda2bd9576676fad3aa61
Eugene Triguba <eugenetriguba@gmail.com>
2020-08-09 19:21:25 -0500

    Print help screen on -h flag

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -9,12 +10,16 @@ import (
 
 func main() {
 	cl := cli.NewCommandLine()
-	_, err := cl.Parse(os.Args[0], os.Args[1:])
+	output, err := cl.Parse(os.Args[0], os.Args[1:])
+	if err == flag.ErrHelp {
+		fmt.Println(output)
+		os.Exit(2)
+	}
+
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Could not parse flags because '%v'\n", err)
 		os.Exit(1)
 	}
 
-	exitCode := cl.Run()
-	os.Exit(exitCode)
+	os.Exit(cl.Run())
 }
